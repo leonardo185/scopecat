fbfd73810ebfbb208047b6a58cd362249ac4d014
Leonardo <leonardo18598@gmail.com>
2021-03-14 05:56:59 +0000

    Add files via upload

---
+++ scopkat.py
@@ -0,0 +1,36 @@
+# reading files
+f1 = open("/home/leonardo/Desktop/Scans/Custom Tools/Outscope.txt", "r")
+f2 = open("/home/leonardo/Desktop/Scans/Custom Tools/Input.txt", "r")
+
+print("Appending.........")
+for line1 in f1:
+    for line2 in f2:
+        #Matching lines from line1 and line2
+        if line1 != line2:
+            f3 = open("/home/leonardo/Desktop/Scans/Custom Tools/Output.txt","a")
+            # f3.write("\n")
+            f3.write(line2)
+            f3.close()
+        else:
+            break
+f1.close()
+f2.close()
+print("Appended")
+print("Sorting.........")
+bands = list()
+
+#read bands from file
+filename = 'Output.txt'
+with open(filename) as fin:
+    for line in fin:
+        bands.append(line.strip())
+
+#sort bands
+bands.sort()
+print(bands)
+
+#write sorted bands
+filename = 'Sorted.txt'
+with open(filename, 'w') as fout:
+    for band in bands:
+        fout.write(band + '\n')
